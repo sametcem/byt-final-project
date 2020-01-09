e1a4e8341c1d19863da25fccc1cafacb993b5140
sametcem <cemhabiboglu@gmail.com>
2020-01-09 22:48:46 +0100

    BYT First Commit

---
+++ article/views.py
@@ -0,0 +1,112 @@
+from django.shortcuts import render,HttpResponse,redirect,get_object_or_404,reverse
+from .forms import ArticleForm
+from .models import Article,Comment
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+"""
+def index(request):
+    #return HttpResponse("Anasayfa") // <h3> Anasayfa </h3>
+    context = {
+        "numbers": [1,2,3,4,5]
+    }
+    return render(request,"index.html",context)
+"""
+#Dynamic URL
+#def detail(request,id):
+#    return HttpResponse("Detail:" + str(id))
+
+def articles(request):
+    keyword = request.GET.get("keyword")
+
+    if keyword:
+        articles = Article.objects.filter(title__contains = keyword)
+        return render(request,"articles.html",{"articles":articles})
+
+    articles = Article.objects.all()
+    return render(request,"articles.html",{"articles":articles})
+
+def index(request):
+    return render(request,"index.html")
+
+def about(request):
+    return render(request,"about.html")
+
+@login_required(login_url = "user:login")
+def dashboard(request):
+    articles = Article.objects.filter(author = request.user)
+    context = {
+        "articles": articles
+    }
+
+    return render(request,"dashboard.html",context)
+@login_required(login_url = "user:login")
+def addarticle(request):
+    form = ArticleForm(request.POST or None,request.FILES or None)
+
+    if form.is_valid():
+        article = form.save(commit=False) #dont save, I will implement myself
+        article.author = request.user
+        article.save()
+
+        """
+        article object
+        you should define author here.
+        article.save()
+        """
+        messages.success(request,"Article is successfully added!")
+        return redirect("article:dashboard")
+
+    return render(request,"addarticle.html",{"form":form})
+
+
+def detail(request,id):
+    #article = Article.objects.filter(id = id).first()
+    article = get_object_or_404(Article,id = id)
+
+    comments = article.comments.all()
+
+    return render(request,"detail.html",{"article":article,"comments":comments})
+
+@login_required(login_url = "user:login")
+def updateArticle(request,id):
+    article = get_object_or_404(Article,id = id)
+    form = ArticleForm(request.POST or None,request.FILES or None,instance=article)
+    if form.is_valid():
+        article = form.save(commit=False)
+        article.author = request.user
+        article.save()
+
+        messages.success(request,"Article is successfully updated!")
+        return redirect("article:dashboard")
+
+    return render(request,"update.html",{"form":form})
+
+@login_required(login_url = "user:login")
+def deleteArticle(request,id):
+    article = get_object_or_404(Article,id = id)
+    article.delete()
+    messages.success(request,"Article is successfully deleted!")
+    return redirect("article:dashboard")
+
+def addComment(request,id):
+    article = get_object_or_404(Article,id = id)
+
+    if request.method == "POST":
+        comment_author = request.POST.get("comment_author")
+        comment_content = request.POST.get("comment_content")
+
+        newComment = Comment(comment_author= comment_author, comment_content= comment_content)
+        newComment.article = article
+        newComment.save()
+    return redirect(reverse("article:detail",kwargs={"id":id}))
+    #/articles/detail/15 -> when we are redirecting dynamic URL
+
+
+
+
+
+
+    
